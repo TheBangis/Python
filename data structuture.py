74159b27ed9ba71ef0f9297cb38b0a57c9ebe47c
Babangida Sani <49342995+TheBangis@users.noreply.github.com>
2023-01-03 15:40:01 -0800

    Add files via upload

---
+++ data structuture.py
@@ -0,0 +1,13 @@
+list_of_random_things = [1, 'a string', True, 8]
+list_of_random_things[0] = 'one'
+print(list_of_random_things[0])
+#above code showed list is a mutable data structure
+
+#print the items data using for loop
+for i in list_of_random_things:
+    print(i)
+    
+#String
+name = 'Babangida'
+print('\n')
+print(name[:4])
